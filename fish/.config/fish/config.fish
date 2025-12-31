4f72a63853464bfc558995267cb0de319b80fa6d
Branden <68295180+brandenarc@users.noreply.github.com>
2025-12-31 14:38:17 +0000

    Add fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,10 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# fastfetch along with the "end" add # in front of #function and #end to bring back fastfetch
+function fish_greeting
+#    # smth smth
+end
+
+# Run A Pokemon Random On Terminal Opening -r randomises 
+pokemon-colorscripts -r --no-title
